927c28890a7b41f62552aa32786dea60aa0e4ac4
Shane Connelly <shane@eskibars.com>
2015-11-23 20:54:38 -0600

    Updated with query example

--- de_dot.pl
+++ de_dot.pl
@@ -1,14 +1,23 @@
 use strict;
 use Search::Elasticsearch;
+use Getopt::Long;
 
 # Note that this doesn't handle external versioning, if you've used that on your docs
 # See https://metacpan.org/pod/Search::Elasticsearch::Bulk#REINDEXING-DOCUMENTS for more info
 
-# Source and destination nodes/indexes
+# Source and destination nodes/indexes -- we'll use command line parameters to override
 my @sourceNodes = ['127.0.0.1:9200'];
 my $sourceIndexName = 'test';
 my @destinationNodes = ['127.0.0.1:9200'];
 my $destinationIndexName = 'test2';
+
+GetOptions ("source=s" => \@sourceNodes,
+              "destination=s" => \@destinationNodes,
+              "verbose"  => \$verbose)
+  or die(usage());
+
+@destinationNodes = split(/,/,join(',',@destinationNodes));
+@sourceNodes = split(/,/,join(',',@sourceNodes));
 
 # Connect to source and destination
 my $source = Search::Elasticsearch->new( nodes => @sourceNodes );
@@ -36,7 +45,8 @@ $destination->bulk_helper(index => $destinationIndexName)->reindex(
     es => $source,
     index => $sourceIndexName,
     size => 1000, # use 1000 docs at a time
-    fields  => ['_source','_parent','_routing'] # make sure to preserve source, parent, and routing values
+    fields => ['_source','_parent','_routing'] # make sure to preserve source, parent, and routing values
+    #, body => { query => { match => { field_name => 123 } } }
   },
   dest_index => $destinationIndexName,
   transform  => sub {
